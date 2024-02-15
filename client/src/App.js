461a9d83e696f02990caa320c21b63b6cb43e141
TejasAU <tejas0418@gmail.com>
2024-02-15 09:55:23 +0530

    added tailwind packages and background

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen bg-cover bg-center bg-[url('/public/images/chair-table-dinning-beach-sea-with-blue-sky.jpg')]">
+      {/* <div className="h-full "> */}
       <h1 className='text-orange-200'>Homepage</h1>
-    </div>
+      {/* </div> */}
+    </div> 
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'home-page-bg': "url('client/public/images/chair-table-dinning-beach-sea-with-blue-sky.jpg')"
+    },
   },
   plugins: [],
 }
