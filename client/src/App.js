c5cb287c3bb30b235516c7c74509a45723fc7341
TejasAU <tejas0418@gmail.com>
2024-02-16 11:17:58 +0530

    bg and tailwind setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-cover h-screen bg-center bg-[url('../public/images/chair-table-dinning-beach-sea-with-blue-sky.jpg')]">
+      <h1 className='text-red-300'>helloworld</h1>
     </div>
   );
 }
