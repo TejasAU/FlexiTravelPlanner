c86cb2d90bae22b0c123ae4a36b1eec7692dc96e
TejasAU <tejas0418@gmail.com>
2024-02-16 14:02:21 +0530

    added UI libraries (Radix)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import { ComplexNavbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className="App bg-cover h-screen bg-center bg-[url('../public/images/chair-table-dinning-beach-sea-with-blue-sky.jpg')]">
-      <h1 className='text-red-300'>helloworld</h1>
+    <div className="bg-cover h-screen bg-[url('../public/images/chair-table-dinning-beach-sea-with-blue-sky.jpg')]">
+      {/* <h1 className='text-red-300'>helloworld</h1> */}
+      {/* <div className=""> */}
+      <ComplexNavbar/>
+      {/* </div> */}
+      <h1>hellowor</h1>
+      
+      
+      
     </div>
   );
 }
