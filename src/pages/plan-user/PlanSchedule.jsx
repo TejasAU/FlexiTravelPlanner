fdbd03936be865296bfb891f61281538ce5cfcec
VK <vishanto95@gmail.com>
2024-04-14 19:52:18 +0530

    Refactoring static data

--- src/pages/plan-user/PlanSchedule.jsx
+++ src/pages/plan-user/PlanSchedule.jsx
@@ -1,45 +1,94 @@
+import { useState } from "react";
+import {
+    TETabs,
+    TETabsContent,
+    TETabsItem,
+    TETabsPane,
+} from "tw-elements-react";
 import DayTimeline from "./DayTimeline";
-import DayTimelinev2 from "./DayTimelinev2";
+import { Link } from "react-router-dom";
 
-export default function PlanSchedule() {
+const tabPanelData = [
+    {
+        dayNum: 1,
+        date: "11/01/2024",
+    },
+    {
+        dayNum: 2,
+        date: "12/01/2024",
+    },
+    {
+        dayNum: 3,
+        date: "13/01/2024",
+    },
+    {
+        dayNum: 4,
+        date: "14/01/2024",
+    },
+    {
+        dayNum: 5,
+        date: "15/01/2024",
+    },
+    {
+        dayNum: 6,
+        date: "16/01/2024",
+    },
+];
+
+const tabContentData = [];
+
+const ExploreCityButton = () => {
     return (
-        <DayTimelinev2 />
-    )
-        {/*
-        <div role="tablist" className="tabs tabs-bordered tabs-lg">
-            <input
-                type="radio"
-                name="my_tabs"
-                role="tab"
-                className="tab"
-                aria-label="Day 1"
-            />
-            <div role="tabpanel" className="tab-content p-10">
-                <DayTimeline test={1} />
-            </div>
-
-            <input
-                type="radio"
-                name="my_tabs"
-                role="tab"
-                className="tab"
-                aria-label="Day 2"
-                checked
-            />
-            <div role="tabpanel" className="tab-content p-10">
-                <DayTimeline test={2} />
-            </div>
-
-            <input
-                type="radio"
-                name="my_tabs"
-                role="tab"
-                className="tab"
-                aria-label="Day 3"
-            />
-            <div role="tabpanel" className="tab-content p-10">
-                <DayTimeline test={3} />
-            </div>
+        <div className="flex items-center justify-center">
+            <button className="btn btn-outline btn-secondary w-2/3">
+                Explore Things to Do
+            </button>
         </div>
-         */}
+    );
+};
+
+export default function DayTimelinev2() {
+    const [justifyActive, setJustifyActive] = useState("tab1");
+
+    const handleJustifyClick = (value) => {
+        if (value === justifyActive) {
+            return;
+        }
+        setJustifyActive(value);
+    };
+
+    return (
+        <div className="mb-3">
+            <TETabs justify>
+                {tabPanelData.map((data, index) => (
+                    <TETabsItem
+                        onClick={() => handleJustifyClick("tab" + { index })}
+                        active={justifyActive === "tab" + { index }}
+                        color="secondary"
+                        className="duration-300"
+                    >
+                        <div className="flex flex-col text-base">
+                            <div>Day {data.dayNum}</div>
+                            <div>{data.date}</div>
+                        </div>
+                    </TETabsItem>
+                ))}
+            </TETabs>
+
+            <TETabsContent>
+                {tabPanelData.map((_, index) => (
+                    <TETabsPane show={justifyActive === "tab" + { index }}>
+                        <Link to={"../../explorecity/1"}>
+                            <div className="flex items-center justify-center">
+                                <button className="btn btn-outline btn-secondary w-2/3">
+                                    Explore Things to Do
+                                </button>
+                            </div>
+                        </Link>
+                        <DayTimeline test={index + 1} />
+                    </TETabsPane>
+                ))}
+            </TETabsContent>
+        </div>
+    );
 }
--- src/pages/plan-user/UserPlans.jsx
+++ src/pages/plan-user/UserPlans.jsx
@@ -1,6 +1,7 @@
 import userPlansOverlay from "../../assets/user-plans-overlay.jpg";
 import { Link } from "react-router-dom";
 import AddPlanModal from "./AddPlanModal";
+import PlusIcon from "../../components/PlusIcon";
 
 const userItineraries = [
     {
@@ -37,6 +38,42 @@ const userItineraries = [
     },
 ];
 
+const PublicIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+        >
+            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+            <path
+                fillRule="evenodd"
+                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+};
+
+const PrivateIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-6 h-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-4.38 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z"
+                clipRule="evenodd"
+            />
+            <path d="m10.748 13.93 2.523 2.523a9.987 9.987 0 0 1-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 0 1 0-1.186A10.007 10.007 0 0 1 2.839 6.02L6.07 9.252a4 4 0 0 0 4.678 4.678Z" />
+        </svg>
+    );
+};
+
 const ItineraryContainer = ({ itinerary }) => {
     const { id, title, desc, isBudgetSet, isListSet, isPublic } = itinerary;
     return (
@@ -70,36 +107,12 @@ const ItineraryContainer = ({ itinerary }) => {
                     {isPublic ? (
                         <div className="flex items-center gap-2">
                             <div>Public</div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                <path
-                                    fillRule="evenodd"
-                                    d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
+                            <PublicIcon />
                         </div>
                     ) : (
                         <div className="flex items-center gap-2">
                             <div>Private</div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-4.38 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z"
-                                    clipRule="evenodd"
-                                />
-                                <path d="m10.748 13.93 2.523 2.523a9.987 9.987 0 0 1-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 0 1 0-1.186A10.007 10.007 0 0 1 2.839 6.02L6.07 9.252a4 4 0 0 0 4.678 4.678Z" />
-                            </svg>
+                            <PrivateIcon />
                         </div>
                     )}
                 </p>
@@ -142,20 +155,7 @@ export default function UserPlans() {
                         className="btn btn-wide btn-outline btn-secondary"
                         onClick={handleAddItinerary}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 4.5v15m7.5-7.5h-15"
-                            />
-                        </svg>
+                        <PlusIcon />
                         Add an Itinerary
                         <AddPlanModal />
                     </button>
